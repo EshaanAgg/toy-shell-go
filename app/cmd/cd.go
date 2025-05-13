a2da2e0235e86ed8b37e062109e44e1e41a12567
EshaanAgg <96648934+EshaanAgg@users.noreply.github.com>
2025-05-13 16:50:50 +0530

    respect the HOME variable

--- app/cmd/cd.go
+++ app/cmd/cd.go
@@ -5,13 +5,21 @@ import (
 	"os"
 )
 
+const HOME_DIRECTORY_MARKER = "~"
+
 func HandleCD(args []string) {
 	if len(args) != 1 {
 		fmt.Printf("usage: cd <path-to-new-dir>, recieved unexpected args: %v\n", args)
 		return
 	}
 
-	err := os.Chdir(args[0])
+	// Get the path to change the directory to
+	path := args[0]
+	if path == HOME_DIRECTORY_MARKER {
+		path = os.Getenv("HOME")
+	}
+
+	err := os.Chdir(path)
 	if err != nil {
 		fmt.Printf("cd: %s: No such file or directory\n", args[0])
 		return
